b2301c0fcaff72fec2a9aa2ade8578eb3ca20c8d
Robbie Cook <robbieck7@gmail.com>
2020-10-08 06:36:32 +1300

    Starting doc refactor

--- markdown.config.js
+++ markdown.config.js
@@ -1,15 +1,12 @@
-const { NodeExtended } = require('./dist/node-extended');
-const fs = require('fs');
-const ENTRY_FILE = "./src/node-extended.ts";
+const { TsDocExtended } = require("tsdoc-extended");
+const fs = require("fs");
 
 /* CLI markdown.config.js file example */
 module.exports = {
   transforms: {
     /* Match <!-- AUTO-GENERATED-CONTENT:START (API) --> */
     API(content, options) {
-      NodeExtended.executeSync(`npx typedoc --plugin typedoc-plugin-markdown src/`);
-      const string = fs.readFileSync('./docs/modules/_node_extended_.md');
-      return string;
+      return TsDocExtended.generateMarkdown('src');
     },
   },
   callback: function () {
